736675851285e38b4354cb43ddabc8ac35fc929e
Gagandeep Singh <gagan.deep@me.com>
2020-08-06 13:57:41 +1000

    Initial Commit

---
+++ CarsalesChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsalesChallenge
+//
+//  Created by Gagandeep Singh on 6/8/20.
+//  Copyright © 2020 Gagandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
